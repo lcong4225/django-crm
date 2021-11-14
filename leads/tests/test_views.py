f3a27161323b60acb1003cd093e646c7664dde66
Cong Le <congle@Congs-MacBook-Pro.local>
2021-11-14 20:41:41 +0900

    Add all my files

---
+++ leads/tests/test_views.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.shortcuts import reverse
+
+
+
+class LandingPageTest(TestCase):
+
+    def test_get(self):
+        response = self.client.get(reverse('landing-page'))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,"landing.html")
+
+    # def test_status_code(self):
+    #     # TODO some sort of test
+    #     response = self.client.get(reverse('landing-page'))
+    #     self.assertEqual(response.status_code,200)
+    #     self.assertTemplateUsed(response,"landing.html")
+        
+
+    # def test_template_name(self):
+    #     response = self.client.get(reverse('landing-page'))
+    #     self.assertTemplateUsed(response,"landing.html")
