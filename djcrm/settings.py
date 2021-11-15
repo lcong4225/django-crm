f52884b393dcfe070e74de7eb7610bdef78b168d
Cong Le <congle@Congs-MacBook-Pro.local>
2021-11-15 10:39:16 +0900

    static

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -156,11 +156,14 @@ USE_TZ = True
 #     BASE_DIR / 'static'
 # ]
 # STATIC_ROOT = "static_root"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = "leads.User"
