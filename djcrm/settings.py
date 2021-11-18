e953b3aa870495bdcb6309f5aeea95bbde95078e
Cong Le <congle@Congs-MacBook-Pro.local>
2021-11-18 14:54:18 +0900

    check

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -156,6 +156,8 @@ elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     }
 
 
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -232,7 +234,7 @@ if not DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = "smtp.gmail.com"
     EMAIL_HOST_USER = "travellife6334@gmail.com"
-    EMAIL_HOST_PASSWORD = "123456"
+    EMAIL_HOST_PASSWORD = "Syncforest"
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     DEFAULT_FROM_EMAIL = 'no-reply <clcrm@gmail.com>'
